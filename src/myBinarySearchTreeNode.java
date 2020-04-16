d90895ab4641ce5c66baffa52ca5c0e90210c6fe
Jacob Esquivel <jesquivel11@miners.utep.edu>
2020-04-15 21:51:23 -0600

    Completed height, depth, size, and constructor methods.

--- src/myBinarySearchTreeNode.java
+++ src/myBinarySearchTreeNode.java
@@ -1,46 +1,120 @@
 class myBinarySearchTreeNode{
+  public static void main(String[] args){
+    int[] A = {4, 3, 7};
+
+    myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A);
+    System.out.println();
+    tree.print();
+    System.out.println(tree.size());
+  }
   int myValue;
   myBinarySearchTreeNode left;
   myBinarySearchTreeNode right;
-    
+
   myBinarySearchTreeNode(int inValue){
     // created a new node with empty child pointers
-
+    myValue = inValue;
+    left = null;
+    right = null;
   }
-  
+
   myBinarySearchTreeNode(int[] A){
     // creates a new Binary Search Tree rooted at the first value in the array
     /// by inserting elements into the tree in the order they are given in A.
-
+    myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A[0]);
+    for(int i = 0; i < A.length; i++){
+      this.insert(A[i]);
+    }
   }
-  
-  public void insert(int inValue){
-    // This method takes a new integer as input and decides 
-    // if it should be place: 
+  public void insert(int inValue) {
+    // This method takes a new integer as input and decides
+    // if it should be place:
     //    * as the left child,
-    //    * as the right child, 
+    //    * as the right child,
     //    * in the left subtree,
     //    * or in the right subtree.
-    // If the value already exists in the tree, no action is taken. 
-    
+    // If the value already exists in the tree, no action is taken.
+    if (inValue > myValue) {
+      if (right == null) {
+        right = new myBinarySearchTreeNode(inValue);
+      }
+      else {
+        right.insert(inValue);
+      }
+    }
+    else{
+      if (inValue <= myValue) {
+        if (left == null) {
+          left = new myBinarySearchTreeNode(inValue);
+        }
+        else {
+          left.insert(inValue);
+        }
+      }
+    }
+    if(inValue == myValue){
+      //Display error message i guess
+      System.out.print("Duplicate found, so no insert.");
+    }
   }
-  
+
   public int height(){
      // This method recursively calculates the height of the entire (sub)tree.
      // This method will take O(n) time
-     return -1;
+    int treeHeight;
+    int leftHeight = 0;
+    int rightHeight = 0;
+
+    if(left != null){
+      leftHeight = left.height();
+    }
+    if(right != null){
+      rightHeight = right.height();
+    }
+
+    if(leftHeight > rightHeight){
+      treeHeight = leftHeight;
+    }
+    else{
+      treeHeight = rightHeight;
+    }
+     return treeHeight + 1;
   }
-  
+
   public int depth(int search){
-     // This method recursively calculates the depth of a given search value. 
-     // If the given value is not in the tree, this method returns -1. 
+     // This method recursively calculates the depth of a given search value.
+     // If the given value is not in the tree, this method returns -1.
      // Note that if the tree is a proper BST, this method should complete in O(log n) time.
-     // Additionally, remember that the depth is the number of nodes on the path from a node to the root 
-     // (i.e. the number of the recursie calls).
+     // Additionally, remember that the depth is the number of nodes on the path from a node to the root
+     // (i.e. the number of the recursive calls).
+    if(search < myValue){
+      if(left != null){
+        return left.depth(search);
+      }
+    }
+    if(search > myValue){
+      if(right != null) {
+        return right.depth(search);
+      }
+    }
+    if(search == myValue){
+      return 0;
+    }
     return -1;
   }
-  
-  // Utility function included so you can debug your solution. 
+  public int size(){
+    int sum = 1;
+
+    if(left != null){
+      sum += left.size();
+    }
+    if(right != null){
+      sum += right.size();
+    }
+    return sum;
+  }
+
+  // Utility function included so you can debug your solution.
   public void print() { print(""); }
   private void print(String prefix) {
     System.out.println(prefix + myValue);
@@ -49,5 +123,4 @@ class myBinarySearchTreeNode{
     if(left != null) left.print(prefix + "\u251C ");
     if(right != null) right.print(prefix + "\u2514 ");
   }
-  
-} 
+}
